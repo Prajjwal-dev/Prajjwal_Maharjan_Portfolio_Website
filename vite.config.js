075881aefa274a5d1f5e9245d859db381510cd6d
Prajjwal Maharjan <prajjwal.maharjan2013@gmail.com>
2026-01-18 23:36:37 +0545

    New_Check

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Prajjwal_Maharjan_Portfolio_Website/",
+  base: "/Prajjwal_Maharjan_Portfolio_Website",
 });
